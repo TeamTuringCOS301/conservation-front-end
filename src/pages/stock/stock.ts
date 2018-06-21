ad5272d579b2377bc468bafa497741d16444917a
Darius Scheepers <dscheepers@irodo.com>
2018-06-21 12:32:51 +0200

    vordering weer

--- src/pages/stock/stock.ts
+++ src/pages/stock/stock.ts
@@ -1,17 +1,56 @@
-//import { Component } from '@angular/core';
-//import { NavController, ToastController } from 'ionic-angular';
-//import { Http, Headers, RequestOptions} from '@angular/http';
-//import { FormGroup, FormControl} from '@angular/forms';
-//import { TabsPage } from '../tabs/tabs';
+import { Component } from '@angular/core';
+import { NavController, ToastController } from 'ionic-angular';
+import { Http, Headers, RequestOptions} from '@angular/http';
+import { FormGroup, FormControl} from '@angular/forms';
 
-/*@Component({
+@Component({
   selector: 'page-stock',
   templateUrl: 'stock.html'
-})*/
+})
 export class StockPage {
     
-    constructor()
+    requestProduct:any;
+    stock:any;
+    product:any;   
+
+    constructor(public http: Http,  public navCtrl: NavController, public toastCtrl: ToastController) 
+    {        
+        this.requestProduct = new FormGroup({
+            name: new FormControl(), 
+            price: new FormControl(),
+            description: new FormControl(),
+            imagePath: new FormControl()
+            });
+
+        this.stock = [];
+        this.product = {};
+        var jsonArr: any = {};
+        jsonArr.location = "";
+        var param = JSON.stringify(jsonArr);
+        let headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+        /*
+
+        let options = new RequestOptions({headers: headers, withCredentials: true});
+        var addr = "http://localhost:8080/stock/list";
+        this.http.get(addr).subscribe
+        (
+            (data) => //Success
+            {
+                var jsonResp = JSON.parse(data.text());
+                //alert(data.text());
+                this.stock = jsonResp.stock;
+            }
+        );
+    }
+
+    /*
+    constructor(public http: Http)
     {
+        
+        
+    }
+    */
 
     }
 
