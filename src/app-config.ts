e8af650094ca2c07ec06815bf11d7e17e027bc6f
Darius Scheepers <dscheepers@irodo.com>
2018-07-20 09:36:05 +0200

    nice login screen. Thanx Tris

--- src/app-config.ts
+++ src/app-config.ts
@@ -1,5 +1,6 @@
 export let CONFIG = {
-    url: "https://192.168.137.1:8080"
+    url: "https://erpcoin.duckdns.org:80"
 }
 // https://erpcoin.duckdns.org:80
 // http://localhost:8080
+// https://192.168.137.1:8080
