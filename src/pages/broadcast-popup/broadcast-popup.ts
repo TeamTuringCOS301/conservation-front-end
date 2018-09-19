e2712adf497f7732b1dd55ea3e225322a308776e
Sewis <van.wyk.sewis@gmail.com>
2018-09-19 12:30:24 +0200

    data validation for alerts

--- src/pages/broadcast-popup/broadcast-popup.ts
+++ src/pages/broadcast-popup/broadcast-popup.ts
@@ -70,23 +70,23 @@ export class BroadcastPopupPage {
             "location":""
         };
 
-        if (value == null)
+        if (value.title == null || value.title == "")
         {
-            if (value.title == null || value.description == null)
-            {
-                alert("Please complete form.");
-                return false;
-            }
-            alert("Please complete form.");
+            alert("Title empty, please complete form.");
             return false;
         }
-
+        if (value.description == null || value.description == "")
+        {
+            alert("Description empty, please complete form.");
+            return false;
+        }
         if (value.severity == null){
             jsonArr.severity = this.alert.severity;
         }
         else{
           jsonArr.severity = parseInt(value.severity);
         }
+
 
         jsonArr.title = value.title;
         jsonArr.description = value.description;
