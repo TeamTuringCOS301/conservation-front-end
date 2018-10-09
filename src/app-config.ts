ab496837ca5390fa63b640115aa51f3eba9f84b4
Darius Scheepers <dscheepers@irodo.com>
2018-10-09 16:46:32 +0200

    pushed Ulrik command

--- src/app-config.ts
+++ src/app-config.ts
@@ -1,7 +1,3 @@
 export let CONFIG = {
-    url: "https://erpcoin.westeurope.cloudapp.azure.com:8080"
+    url: "/api"
 }
-// https://erpcoin.duckdns.org:80
-// http://localhost:8080
-// https://192.168.137.73:8080
-// haha
